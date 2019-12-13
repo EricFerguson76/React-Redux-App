032fb5ea207b2e1e5350417a17899bf99b469e85
Eric Ferguson <turtle0888@gmail.com>
2019-12-12 18:14:37 -0600

    Add axios

--- react-redux-app/src/actions/index.js
+++ react-redux-app/src/actions/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-export const FETCH_CARD_START = 'FETCH_CARD_START';
-export const FETCH_CARD_SUCCESS = 'FETCH_CARD_SUCCESS';
-export const FETCH_CARD_FAILURE = 'FETCH_CARD_FAILURE';
+export const FETCH_CARD_START = 'FETCH_QUOTE_START';
+export const FETCH_CARD_SUCCESS = 'FETCH_QOUTE_SUCCESS';
+export const FETCH_CARD_FAILURE = 'FETCH_QOUTE_FAILURE';
 
 
 export const getCard = () => dispatch => {
     dispatch({ type: FETCH_CARD_START });
-    axios.get('https://www.breakingbadapi.com/api/characters')
+    axios.get('https://quote-garden.herokuapp.com/quotes/random')
     .then(res => {
         return( console.log(res))
     })
--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-import { reducers } from './reducers';
+import reducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducers,
+  reducer,
   composeEnhancers(applyMiddleware(thunk, logger))
 );
 
